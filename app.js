bcdc52f61b88ce7c9e0b21c34429b4251c9548f4
eppixdev <hatkoff@gmail.com>
2018-04-30 19:26:35 -0700

    Added search functionality

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 // Init MovieDB
 const movieDatabase = new MovieDatabase;
 
-const ui = new UI;
+// Search input
+const searchMovie = document.getElementById('searchMovie');
 
-movieDatabase.getMovie()
-  .then(data => {
-    console.log(data.movie.title);
-    console.log(data.movie.tagline);
-    console.log(data.movie.overview);
-    console.log(data.movie.release_date);
-    console.log(data.movie.runtime);
-    console.log(data.movie.revenue);
-    ui.showProfile();
-  })
+// Search input event listener
+searchMovie.addEventListener('keyup', (e) => {
+  const movieText = e.target.value;
+
+  movieDatabase.searchMovie(movieText)
+    .then(data => {
+      const movieID = data.movie.results[0].id
+      return movieDatabase.getMovie(movieID)
+        .then(data => {
+          console.log(data);
+          const movieProfile = document.getElementById('movieProfile');
+          movieProfile.innerHTML = `<h1>${data.movie.title}</h1>
+          <p>${data.movie.tagline}</p>
+          <p>${data.movie.release_date}`;
+        })
+    })
+})
--- movieDB.js
+++ movieDB.js
@@ -3,10 +3,20 @@ class MovieDatabase {
     this.apiKey = 'a5fd48a05497bb21e69e61b0c54a7fad';
   }
 
-  async getMovie() {
-    const movieResponse = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=${this.apiKey}`);
+  async searchMovie(searchQuery) {
+    const movieSearch = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${searchQuery}`);
 
-    const movie = await movieResponse.json();
+    const movie = await movieSearch.json();
+
+    return {
+      movie
+    }
+  }
+
+  async getMovie(id) {
+    const movieProfile = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${this.apiKey}`);
+
+    const movie = await movieProfile.json();
 
     return {
       movie
